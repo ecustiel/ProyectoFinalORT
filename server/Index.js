6ba14a27f930c611cf14122ed817177f1d5b4813
EduardoCustiel <ecustiel@gmail.com>
2022-11-02 18:32:53 -0300

    Actualizando Componentes del Server

--- server/Index.js
+++ server/Index.js
@@ -1,1 +1,24 @@
-console.log("Hola Mundo");
+const express = require("express");
+require('dotenv').config();
+
+
+//Creo servidor de express
+const app = express();
+
+
+//Directorio Publico
+app.use(express.static('public'))
+
+//Parseo de Body
+app.use(express.json());
+
+
+//Rutas
+app.use('/api/auth', require('./Routes/Auth'));
+
+
+//Peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor Corriendo en puerto ${process.env.PORT}`)
+});
+
