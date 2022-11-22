da17e9370bd1d51db60957fcd8448a22db873a8a
EduardoCustiel <ecustiel@gmail.com>
2022-11-22 00:58:57 -0300

    Datos Faltantes

--- client/src/Routers/AppRouter.js
+++ client/src/Routers/AppRouter.js
@@ -1,58 +1,58 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React, { useEffect } from 'react'
-import {BrowserRouter as Router, Routes, Route, Redirect} from 'react-router-dom'
-import Layout from '../Components/Layouts/Layout'
-import { useAuthStore } from '../Hooks/useAuthStore'
-import HomePage from '../Pages/HomePage'
-import LoginPage from '../Pages/LoginPage'
-import NotFoundPage from '../Pages/NotFoundPage'
-import PerfilPage from '../Pages/PerfilPage'
-import RegisterPage from '../Pages/RegisterPage'
-import SearchPage from '../Pages/SearchPage'
-import RegisterProductPage from '../Pages/RegisterProduct/RegisterProductPage'
-
-
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Layout from "../Components/Layouts/Layout";
+import { useAuthStore } from "../Hooks/useAuthStore";
+import HomePage from "../Pages/HomePage";
+import LoginPage from "../Pages/LoginPage";
+import NotFoundPage from "../Pages/NotFoundPage";
+import PerfilPage from "../Pages/PerfilPage";
+import RegisterPage from "../Pages/RegisterPage";
+import SearchPage from "../Pages/SearchPage";
+import RegisterProductPage from "../Pages/RegisterProduct/RegisterProductPage";
 
 export default function AppRouter() {
-
-  const {status, checkAuthToken} = useAuthStore();
+  const { status, checkAuthToken } = useAuthStore();
 
   useEffect(() => {
     checkAuthToken();
-  }, [])
+  }, []);
 
-  if(status === 'checking') {
-    return (
-      <h3>Cargando...</h3>
-    )
+  if (status === "checking") {
+    return <h3>Cargando...</h3>;
   }
-
-
 
   return (
     <Router>
-    <Layout>
-      
+      <Layout>
         <Routes>
-
-            {status === 'not-authenticated' && <>
-            <Route exact path='/login' element = {<LoginPage />} />
-            <Route exact path='/register' element = {<RegisterPage />} />
-            
+          {status === "not-authenticated" && (
+            <>
+              <Route exact path="/login" element={<LoginPage />} />
+              <Route exact path="/register" element={<RegisterPage />} />
             </>
-          }
-
-          {status === 'authenticated' && <>
-            <Route exact path='/perfil' element = {<PerfilPage />} />
-            <Route exact path='/search' element = {<SearchPage />} />
+          )}
+          {status === "authenticated" && (
+            <>
+              <Route exact path="/perfil" element={<PerfilPage />} />
+              <Route exact path="/search" element={<SearchPage />} />
             </>
-          }
-            
-            <Route exact path='/registerProduct' element = {<RegisterProductPage />} />
-            <Route exact path='/' element = {<HomePage />} />
-            <Route exact path='*' element = {<NotFoundPage />} /> //Error 404 page not found
+          )}
+          <Route
+            exact
+            path="/registerProduct"
+            element={<RegisterProductPage />}
+          />
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="*" element={<NotFoundPage />} /> //Error 404 page
+          not found
         </Routes>
-    </Layout>
+      </Layout>
     </Router>
-  )
+  );
 }
