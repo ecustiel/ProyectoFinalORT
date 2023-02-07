bc3e5bd0d5b1e749eacca519c81e0ce2b07805f6
EduardoCustiel <ecustiel@gmail.com>
2023-02-06 23:36:25 -0300

    Sistema de Gestion agregado a lista completo, Falta a base!

--- client/src/Pages/HouseItemsControl/ItemsControlPage.js
+++ client/src/Pages/HouseItemsControl/ItemsControlPage.js
@@ -1,9 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useRegisterPubStore } from "../../Hooks/useRegisterPubStore";
+import { Button } from "react-bootstrap";
+import { Toast } from "../../Helpers/SwalHelpers";
+import { SyncLoader } from "react-spinners";
+import { useSelector } from "react-redux";
+import ItemToList from "./ItemToList";
+import { Panel } from "rsuite";
+import "./ItemsControlPage.css";
+import ItemsQuantityAndPrice from "../HouseItemsControl/ItemsQuantityAndPrice/ItemsQuantityAndPrice";
+import ItemsQuantityAndPriceList from "./ItemsQuantityAndPrice/ItemsQuantityAndPriceList";
 
 const ItemsControlPage = () => {
+  const { user } = useSelector((state) => state.auth);
+  const { getPublications } = useRegisterPubStore();
+  const [dataPublicationsToWork, setDataPublicationsToWork] = useState([]);
+  const [dataPublications, setDataPublications] = useState([]);
+  const [trueOrFalse, setTrueOrFalse] = useState(false);
+  const [showPanel, setShowPanel] = useState(false);
+
+  //Lista Productos
+  const [items, setItems] = useState([]);
+  const [idPublicacion, setIdPublicacion] = useState();
+  const [contador, setContador] = useState(100);
+
+  //arreglar que si id es distinto borre la lista
+  const addItem = (item) => {
+    if (showPanel) {
+      setContador(contador + 1);
+      setItems([...items, item]);
+    } else {
+      Toast.fire({
+        title: "Error!",
+        text: "Seleccione una Propiedad!",
+        icon: "error",
+        confirmButtonText: "Ok!",
+      });
+    }
+  };
+
+  const borrarItem = (item) => {
+    setItems(items.filter((dataItem) => dataItem.cont !== item.cont));
+  };
+
+  const setPublications = async () => {
+    await getPublications().then((item) => {
+      setDataPublicationsToWork(item.publicationsFromDB);
+    });
+    setTrueOrFalse(true);
+  };
+
+  const filterDataPublications = () => {
+    dataPublicationsToWork.filter((item) => {
+      if (item.idUser === user.uid && dataPublications.indexOf(item) < 0) {
+        setDataPublications((dataPublications) => [...dataPublications, item]);
+      }
+    });
+    setTrueOrFalse(false);
+  };
+
+  useEffect(() => {
+    setPublications();
+
+    filterDataPublications();
+  }, []);
+
+  const ShowMeTheTrue = (e) => {
+    e.preventDefault();
+    setIdPublicacion(e.target.id);
+    console.log(e.target.id);
+    setShowPanel(true);
+  };
+
+  console.log(items);
+  console.log(contador);
+
   return (
     <div>
-      <h1>Probando</h1>
+      <h2>Seleccione Una de Sus Publicaciones!</h2>
+      <Panel className="panel-arriba" shaded>
+        {dataPublicationsToWork.length > 0 ? (
+          <ItemToList
+            dataPublicationsToWork={dataPublicationsToWork}
+            ShowMeTheTrue={ShowMeTheTrue}
+          />
+        ) : (
+          <SyncLoader
+            color={"#E08E45"}
+            loading={true}
+            size={10}
+            margin={10}
+            speedMultiplier={1}
+            className="ringClassNo"
+          />
+        )}
+      </Panel>
+      <Panel className="panel-abajo" shaded>
+        <ItemsQuantityAndPrice
+          addItemFun={addItem}
+          idPublicacion={idPublicacion}
+          contador={contador}
+        />
+      </Panel>
+      <Panel className="panel-derecha" shaded>
+        {items.map((item) => (
+          <ItemsQuantityAndPriceList
+            item={item}
+            key={contador}
+            borrarItemList={borrarItem}
+          />
+        ))}
+
+        <div className="d-grid gap-2">
+          <Button variant="info" className="mt-3" size="lg">
+            {" "}
+            Agregar Lista a Propiedad!{" "}
+          </Button>
+        </div>
+      </Panel>
     </div>
   );
 };
---
+++ client/src/Pages/HouseItemsControl/ItemsQuantityAndPrice/ItemsQuantityAndPrice.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+const ItemsQuantityAndPrice = ({ addItemFun, idPublicacion, contador }) => {
+  //Lista de Items
+  const [nombre, setNombre] = useState("");
+  const [cantidad, setCantidad] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [cont, setCont] = useState(contador);
+  const [idPublicacionToSend, setIdPublicacionToSend] = useState(idPublicacion);
+
+  const addItem = (_) => {
+    addItemFun({
+      id: idPublicacion,
+      cont: contador,
+      nombre,
+      cantidad,
+      precio,
+    });
+    setNombre("");
+    setCantidad("");
+    setPrecio("");
+  };
+
+  return (
+    <div>
+      <Form.Group className="mb-3" id="title">
+        <Form.Label>
+          <strong>
+            <h2>Lista de Productos</h2>
+          </strong>
+          <p>
+            A continuacion, ingrese una lista de productos que seran asociados a
+            la propiedad que eligio. Esto luego servira de Control a la hora de
+            que la propiedad sea entregada devuelta!
+          </p>
+        </Form.Label>
+        <Form.Label>
+          <strong>Nombre del Item:</strong>
+        </Form.Label>
+        <Form.Control
+          name="name"
+          placeholder="Nombre del Producto"
+          id="name"
+          value={nombre}
+          onChange={(e) => {
+            setNombre(e.target.value);
+          }}
+        />
+
+        <Form.Label>
+          <strong>Cantidad:</strong>
+        </Form.Label>
+        <Form.Control
+          name="quantity"
+          placeholder="Cantidad"
+          id="quantity"
+          value={cantidad}
+          onChange={(e) => {
+            setCantidad(e.target.value);
+          }}
+        />
+
+        <Form.Label>
+          <strong>Precio (Por Unidad):</strong>
+        </Form.Label>
+        <Form.Control
+          name="price"
+          placeholder="Precio (Por Unidad)"
+          id="price"
+          value={precio}
+          onChange={(e) => {
+            setPrecio(e.target.value);
+          }}
+        />
+
+        <Button variant="success" className="mt-3" onClick={addItem}>
+          {" "}
+          Agregar Item!{" "}
+        </Button>
+      </Form.Group>
+    </div>
+  );
+};
+
+export default ItemsQuantityAndPrice;
